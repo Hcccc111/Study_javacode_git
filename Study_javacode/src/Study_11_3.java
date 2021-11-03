3dd5111147a3cea397981f912e6468db37c3ca39
Hcccc111 <hc1160992614@163.com>
2021-11-03 21:36:06 +0800

    学习11.3

---
+++ Study_javacode/src/Study_11_3.java
@@ -0,0 +1,70 @@
+
+import java.util.Scanner;
+public class Study_11_3 {
+//	1.public static void main(String[] args) {
+//		// 计算圆柱体积，Π*r*r*h
+//		Scanner input = new Scanner(System.in);
+//		System.out.println("请输入圆柱高");
+//		int h = input.nextInt();
+//		System.out.println("请输入圆柱半径");
+//		
+//		int r=input.nextInt();
+//		System.out.println("体积是"+3.14*r*r*h);
+//
+//	}
+//2.public static void main(String[] args) {
+//	//定义一个整数计算各位值
+//	int num = 54321;
+//	int w=num/10000;
+//	int q=num/1000%10;
+//	int b=num/100%10;
+//	int s=num/10%10;
+//	int g=num/1%10;
+//	System.out.println(w+q+b+s+g);
+//
+//	}
+//}
+//	3.public static void main(String[] args) {
+//	//输入天数 根据天数计算周数和剩余天数
+//		Scanner input= new Scanner(System.in);
+//		System.out.println("请输入天数");
+//		int day = input.nextInt();
+//		int week = day/7;
+//		System.out.println("周数是"+week);
+//		int lessday = day%7;
+//		System.out.println("剩余周数是"+lessday);
+//		
+//	}
+//	
+//	
+//	
+public static void main(String[] args) {
+
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
